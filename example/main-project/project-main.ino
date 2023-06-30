9d8779051c1abc921db52f79e44b891ce0aa8908
anuragpatle <anurag.ap6@gmail.com>
2023-06-30 19:02:23 +0530

    Implemented logic for wifi and condition for ...
    Implemented logic for wifi and condition for screen switching

--- example/main-project/project-main.ino
+++ example/main-project/project-main.ino
@@ -4,6 +4,9 @@
 #include "pin_config.h"
 #include "station_1.h"
 
+#include <WiFi.h>
+#include <HTTPClient.h>
+
 //-------------------------------------Display images
 
 // JPEG decoder library
@@ -12,22 +15,14 @@
 // Return the minimum of two values a and b
 #define minimum(a, b) (((a) < (b)) ? (a) : (b))
 
-// Include the sketch header file that contains the image stored as an array of bytes
-// More than one image array could be stored in each header file.
-#include "jpeg1.h"
-#include "jpeg2.h"
-#include "jpeg3.h"
-#include "jpeg4.h"
-
-// Count how many times the image is drawn for test purposes
-uint32_t icount = 0;
-//----------------------------------------------------------------------------------------------------
-//-------------------------------------Display images (end)
-
 //---------------------------Analog Voltage Read for solar panel
+const char *READINGS_AND_QR_SCREEN = "READINGS_AND_QR_SCREEN";
+const char *WHILE_CHARGING_SCREEN = "WHILE_CHARGING_SCREEN";
+
 int solarPanelPin = 1;
 float energyGenerated = 10; // in KWh
-
+const char *SCREEN_MODE = READINGS_AND_QR_SCREEN;
+bool toggleScreenMode = 'true';
 //---------------------------Analog Voltage Read for solar panel
 
 /* The product now has two screens, and the initialization code needs a small change in the new version. The LCD_MODULE_CMD_1 is used to define the
@@ -64,12 +59,28 @@ lcd_cmd_t lcd_st7789v[] = {
 };
 #endif
 
+const char *ssid = "CWLANGuest";
+const char *password = "welcomeguest";
+
+// Your Domain name with URL path or IP address with path
+const char *serverName = "20.96.116.65:80/api/v1/updateStationState/2";
+
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastTime = 0;
+// Timer set to 10 minutes (600000)
+// unsigned long timerDelay = 600000;
+// Set timer to 5 seconds (5000)
+unsigned long timerDelay = 5000;
+
 void setup()
 {
+
+  Serial.begin(115200);
+
   pinMode(PIN_POWER_ON, OUTPUT);
   digitalWrite(PIN_POWER_ON, HIGH);
 
-  Serial.begin(115200);
   Serial.println("Hello T-Display-S3");
   tft.setTextSize(2); // Set the text size
   tft.setTextDatum(MC_DATUM);
@@ -93,201 +104,84 @@ void setup()
 #endif
 
   tft.setRotation(0);
-  tft.setSwapBytes(true);
-  tft.pushImage(0, 0, 320, 170, (uint16_t *)img_logo);
-  delay(2000);
+  tft.fillScreen(TFT_BLACK);
+  // tft.setSwapBytes(true);
+  // tft.pushImage(0, 0, 320, 170, (uint16_t *)img_logo);
+  // delay(2000);
 
-  ledcSetup(0, 2000, 8);
-  ledcAttachPin(PIN_LCD_BL, 0);
-  ledcWrite(0, 255);
+  // ledcSetup(0, 2000, 8);
+  // ledcAttachPin(PIN_LCD_BL, 0);
+  // ledcWrite(0, 255);
 }
 
 void loop()
 {
 
-  tft.setRotation(0); // landscape
-  tft.fillScreen(TFT_BLACK);
+  // Connect to Wi-Fi network
 
-  // Draw a black rectangle on the right half of the screen
-  tft.fillRect(0, tft.height() / 2 + 22, tft.width(), tft.height() / 2 + 22, TFT_LIGHTGREY);
-  drawArrayJpeg(station_1, sizeof(station_1), 26.5, 186); // Draw a jpeg image stored in memory
-                                                          // delay(5000000); // Delay for a short period of time
-  // tft.drawLine(0, 180, tft.width(), 180, TFT_SKYBLUE);
-  // tft.drawLine(0, 179, tft.width(), 179, TFT_SKYBLUE);
-  // tft.drawLine(0, 178, tft.width(), 178, TFT_SKYBLUE);
-  tft.drawLine(0, 72, tft.width(), 72, TFT_SKYBLUE);
-
-  targetTime = millis();
-
-  while (true)
+  Serial.println(WiFi.status()); // should be equal to 3
+  while (WiFi.status() != WL_CONNECTED)
   {
-    // Solar Panel
-    float power = analogRead(solarPanelPin) * 2.5 / 4095; // Read voltage from ADC
-    tft.setTextColor(TFT_DARKGREY, TFT_BLACK);
-    tft.drawString("Solar Power", 72, 12, 1);
-    tft.setTextColor(TFT_GREEN, TFT_BLACK);
-    tft.drawFloat(power, 4, 50, 38, 2);
-    tft.setTextColor(TFT_DARKGREY, TFT_BLACK);
-    tft.drawString("kW", 142, 40, 2);
-
-    // Station Energy Available
-    tft.setTextColor(TFT_DARKGREY, TFT_BLACK);
-    tft.drawString("Available", 60, 102, 1);
-    tft.drawString("Energy", 42, 120, 1);
-
-    // energy produced in one second
-    energyGenerated = energyGenerated + (power / 3600); //  1 hour = 3600 second. Since, loop is running in each second
-    tft.setTextColor(TFT_BROWN, TFT_BLACK);
-    tft.drawFloat(energyGenerated, 4, 53, 145, 2);
-    tft.setTextColor(TFT_DARKGREY, TFT_BLACK);
-    tft.drawString("kWh", 145, 147, 2);
-
-    // QR: Scan me to FUEL
-    tft.setTextSize(2);
-    tft.setTextColor(TFT_BACKLIGHT_ON, TFT_LIGHTGREY);
-    tft.drawString("SCAN TO FUEL", 85, tft.height() - 10 , 1);
-
-    delay(500);
+    Serial.println("Connecting to WiFi...");
+    WiFi.begin(ssid, password);
+    delay(20000);
   }
 
-  delay(5000000); // Delay for a short period of time
+  Serial.println("Connected to WiFi");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 
-  // First we test them with a background colour set
-  // tft.setTextSize(1);
-  tft.fillScreen(TFT_WHITE);
-  tft.setTextColor(TFT_BLACK, TFT_WHITE);
-  tft.drawString("Available", 0, 48, 2);
-  int xpos = 0;
-  // xpos += tft.drawString("xyz{|}~", 0, 64, 2);
-  tft.drawChar(127, xpos, 64, 2);
-  delay(WAIT);
+  if (strcmp(SCREEN_MODE, READINGS_AND_QR_SCREEN) == 0 && toggleScreenMode == true)
+  {
 
-  tft.fillScreen(TFT_WHITE);
-  tft.setTextColor(TFT_BROWN, TFT_WHITE);
+    toggleScreenMode = false;
+    // Draw a black rectangle on the right half of the screen
+    tft.fillRect(0, tft.height() / 2 + 22, tft.width(), tft.height() / 2 + 22, TFT_LIGHTGREY);
+    drawArrayJpeg(station_1, sizeof(station_1), 26.5, 186); // Draw a jpeg image stored in memory
+                                                            // delay(5000000); // Delay for a short period of time
+    // tft.drawLine(0, 180, tft.width(), 180, TFT_SKYBLUE);
+    // tft.drawLine(0, 179, tft.width(), 179, TFT_SKYBLUE);
+    // tft.drawLine(0, 178, tft.width(), 178, TFT_SKYBLUE);
+    tft.drawLine(0, 72, tft.width(), 72, TFT_SKYBLUE);
+  }
 
-  tft.drawString("KWh", 0, 104, 4);
-  delay(WAIT);
+  if (strcmp(SCREEN_MODE, WHILE_CHARGING_SCREEN) == 0 && toggleScreenMode == true)
+  {
+    toggleScreenMode = false;
 
-  tft.fillScreen(TFT_WHITE);
-  tft.drawString("sometxt", 0, 0, 4);
-  xpos = 0;
-  // xpos += tft.drawString("{|}~", 0, 78, 4);
-  tft.drawChar(127, xpos, 78, 4);
-  delay(WAIT);
+  }
 
-  tft.fillScreen(TFT_WHITE);
-  tft.setTextColor(TFT_BLACK, TFT_WHITE);
+  // targetTime = millis();
 
-  tft.drawString("012345", 0, 0, 6);
-  tft.drawString("6789", 0, 40, 6);
-  tft.drawString("apm-:.", 0, 80, 6);
-  delay(WAIT);
+  // while (true)
+  // {
+  // Solar Panel
+  float power = analogRead(solarPanelPin) * 2.5 / 4095; // Read voltage from ADC
+  tft.setTextColor(TFT_DARKGREY, TFT_BLACK);
+  tft.drawString("Solar Power", 72, 12, 1);
+  tft.setTextColor(TFT_GREEN, TFT_BLACK);
+  tft.drawFloat(power, 4, 50, 38, 2);
+  tft.setTextColor(TFT_DARKGREY, TFT_BLACK);
+  tft.drawString("kW", 142, 40, 2);
 
-  tft.fillScreen(TFT_WHITE);
-  tft.setTextColor(TFT_BLACK, TFT_WHITE);
+  // Station Energy Available
+  tft.setTextColor(TFT_DARKGREY, TFT_BLACK);
+  tft.drawString("Available", 60, 102, 1);
+  tft.drawString("Energy", 42, 120, 1);
 
-  tft.drawString("0123", 0, 0, 7);
-  tft.drawString("4567", 0, 60, 7);
-  delay(WAIT);
+  // energy produced in one second
+  energyGenerated = energyGenerated + (power / 3600); //  1 hour = 3600 second. Since, loop is running in each second
+  tft.setTextColor(TFT_BROWN, TFT_BLACK);
+  tft.drawFloat(energyGenerated, 4, 53, 145, 2);
+  tft.setTextColor(TFT_DARKGREY, TFT_BLACK);
+  tft.drawString("kWh", 145, 147, 2);
 
-  tft.fillScreen(TFT_WHITE);
-  tft.drawString("890:.", 0, 0, 7);
-  tft.drawString("", 0, 60, 7);
-  delay(WAIT);
+  // QR: Scan me to FUEL
+  tft.setTextSize(2);
+  tft.setTextColor(TFT_BACKLIGHT_ON, TFT_LIGHTGREY);
+  tft.drawString("SCAN TO FUEL", 85, tft.height() - 10, 1);
 
-  tft.fillScreen(TFT_WHITE);
-  tft.setTextColor(TFT_BLACK, TFT_WHITE);
-
-  tft.drawString("01", 0, 0, 8);
-  delay(WAIT);
-
-  tft.drawString("23", 0, 0, 8);
-  delay(WAIT);
-
-  tft.drawString("45", 0, 0, 8);
-  delay(WAIT);
-
-  tft.drawString("67", 0, 0, 8);
-  delay(WAIT);
-
-  tft.drawString("89", 0, 0, 8);
-  delay(WAIT);
-
-  tft.drawString("0:.", 0, 0, 8);
-  delay(WAIT);
-
-  tft.setTextColor(TFT_MAGENTA);
-  tft.drawNumber(millis() - targetTime, 0, 100, 4);
-  delay(4000);
-
-  // Now test them with transparent background
-  targetTime = millis();
-
-  // tft.setTextSize(1);
-  // tft.fillScreen(TFT_WHITE);
-  // tft.setTextColor(TFT_BLACK, TFT_WHITE);
-
-  // tft.drawString(" !\"#$%&'()*+,-./0123456", 0, 0, 2);
-  // tft.drawString("789:;<=>?@ABCDEFGHIJKL", 0, 16, 2);
-  // tft.drawString("MNOPQRSTUVWXYZ[\\]^_`", 0, 32, 2);
-  // tft.drawString("abcdefghijklmnopqrstuvw", 0, 48, 2);
-  // xpos = 0;
-  // xpos += tft.drawString("xyz{|}~", 0, 64, 2);
-  // tft.drawChar(127, xpos, 64, 2);
-  // delay(WAIT);
-
-  // tft.fillScreen(TFT_WHITE);
-  // tft.setTextColor(TFT_BLACK, TFT_WHITE);
-
-  // tft.drawString(" !\"#$%&'()*+,-.", 0, 0, 4);
-  // tft.drawString("/0123456789:;", 0, 26, 4);
-  // tft.drawString("<=>?@ABCDE", 0, 52, 4);
-  // tft.drawString("FGHIJKLMNO", 0, 78, 4);
-  // tft.drawString("PQRSTUVWX", 0, 104, 4);
-
-  // delay(WAIT);
-  tft.fillScreen(TFT_WHITE);
-  tft.drawString("pqrstuvwxyz", 0, 52, 4);
-  xpos = 0;
-  xpos += tft.drawString("{|}~", 0, 78, 4);
-  tft.drawChar(127, xpos, 78, 4);
-  delay(WAIT);
-
-  // tft.fillScreen(TFT_WHITE);
-  tft.setTextColor(TFT_BLUE);
-
-  tft.drawString("apm-:. 99 ", 0, 80, 6);
-  delay(WAIT);
-
-  // tft.fillScreen(TFT_WHITE);
-  tft.setTextColor(TFT_BLACK, TFT_WHITE);
-
-  tft.drawString("0123", 0, 0, 7);
-  delay(WAIT);
-
-  // tft.fillScreen(TFT_WHITE);
-  tft.drawString("890:.", 0, 0, 7);
-  tft.drawString("", 0, 60, 7);
-  delay(WAIT);
-
-  // tft.fillScreen(TFT_WHITE);
-  tft.setTextColor(TFT_BLACK, TFT_WHITE);
-
-  tft.drawString("0123", 0, 0, 8);
-  delay(WAIT);
-
-  // tft.fillScreen(TFT_WHITE);
-  tft.drawString("4567", 0, 0, 8);
-  delay(WAIT);
-
-  // tft.fillScreen(TFT_WHITE);
-  tft.drawString("890:.", 0, 0, 8);
-  delay(WAIT);
-
-  tft.setTextColor(TFT_MAGENTA, TFT_WHITE);
-
-  tft.drawNumber(millis() - targetTime, 0, 100, 4);
-  delay(4000);
+  delay(500);
 }
 
 // ####################################################################################################
@@ -435,16 +329,16 @@ void jpegInfo()
 
 // The Due will work fine with the HX8357_Due library.
 
-void showTime(uint32_t msTime)
-{
-  // tft.setCursor(0, 0);
-  // tft.setTextFont(1);
-  // tft.setTextSize(2);
-  // tft.setTextColor(TFT_WHITE, TFT_BLACK);
-  // tft.print(F(" JPEG drawn in "));
-  // tft.print(msTime);
-  // tft.println(F(" ms "));
-  Serial.print(F(" JPEG drawn in "));
-  Serial.print(msTime);
-  Serial.println(F(" ms "));
-}
+// void showTime(uint32_t msTime)
+// {
+//   // tft.setCursor(0, 0);
+//   // tft.setTextFont(1);
+//   // tft.setTextSize(2);
+//   // tft.setTextColor(TFT_WHITE, TFT_BLACK);
+//   // tft.print(F(" JPEG drawn in "));
+//   // tft.print(msTime);
+//   // tft.println(F(" ms "));
+//   Serial.print(F(" JPEG drawn in "));
+//   Serial.print(msTime);
+//   Serial.println(F(" ms "));
+// }
